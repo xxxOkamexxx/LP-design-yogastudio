037eee969d7b857609bba8ded07802e8b943a70e
Chikage Molander <chikage.t.molander@gmail.com>
2023-11-03 14:22:55 +0100

    added javascript for the cards

--- assets/posts.js
+++ assets/posts.js
@@ -2,26 +2,31 @@ const posts = [
   {
     'id':'01',
     'post': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores inventore pariatur unde explicabo distinctio ex deserunt, asperiores odit nulla eligendi repellendus tempora odio eum nesciunt beatae perferendis a vel, minus omnis. Provident necessitatibus aliquam praesentium minima nisi libero, corporis soluta quis veritatis sit magni? Deleniti placeat dolor qui inventore esse, facilis et, explicabo exercitationem nisi laudantium voluptas, laborum dolorem consectetur sed soluta repudiandae repellendus quidem ea. Dicta dolore pariatur natus magni reprehenderit cumque quidem dolores? Eum, excepturi laboriosam placeat dicta obcaecati repellendus amet cum soluta ducimus praesentium suscipit similique ipsam molestias exercitationem cupiditate quis culpa rerum sed nesciunt pariatur inventore voluptatum ad quasi. Recusandae nesciunt, commodi explicabo nemo, mollitia sit et tempora deleniti asperiores itaque unde consequatur facilis debitis minus similique voluptas corrupti, nobis nulla rerum quam eligendi dolore! Nobis a expedita debitis, modi eveniet vitae deleniti asperiores unde consequuntur.',
-    'image': 'assets/images/post01.jpg'
+    'image': 'assets/images/post01.jpg',
+    'date':'6 hours'
   },
   {
     'id':'02',
     'post': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error eligendi quas praesentium rerum iure pariatur nulla. Cupiditate asperiores explicabo ullam ratione deserunt! Mollitia veniam illum asperiores sunt ducimus magni nemo expedita maiores. Neque magnam a reprehenderit dolores animi harum officia voluptatum, veniam nostrum corporis, alias officiis provident eligendi corrupti quae, eaque quod! Et nobis mollitia odio tempora expedita assumenda eum modi aliquam, deleniti, voluptatum itaque sapiente cumque rem? Quasi officia, cupiditate non eum temporibus nisi eligendi laboriosam animi eius qui iure totam hic ad exercitationem illum sapiente eaque nesciunt, vero commodi sunt explicabo magnam? Nam autem dolorum quae ab quibusdam nemo voluptates delectus, iusto nostrum ullam consectetur voluptatibus atque doloremque assumenda expedita aut saepe quaerat cupiditate veritatis quod, ad veniam.',
-    'image': 'assets/images/post02.jpg'
+    'image': 'assets/images/post02.jpg',
+    'date':'1 day'
   },
   {
     'id':'03',
     'post': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore suscipit, totam perspiciatis quis a natus omnis, ipsam sint illo sapiente ipsum in consequatur porro non culpa assumenda eius laudantium est? Ab illum fuga ratione doloremque magni odio ducimus quia eos magnam vitae nam aut veniam, nostrum ullam quos quidem ipsum deserunt molestiae quaerat minima reprehenderit asperiores itaque! Corrupti, atque accusamus ea necessitatibus vero ipsum, deserunt nostrum quam nihil maiores incidunt officia cum non qui vitae ut culpa iusto aliquid officiis molestias? Suscipit blanditiis, culpa sunt atque ducimus numquam sapiente facere, perspiciatis veritatis reiciendis mollitia veniam, optio autem quas? Sequi, tenetur exercitationem debitis illum impedit odio incidunt ipsam eos totam fugiat accusantium sit excepturi quo? Dolor velit, necessitatibus sint ducimus temporibus molestias quibusdam.',
-    'image': 'assets/images/post03.jpg'
+    'image': 'assets/images/post03.jpg',
+    'date':'2 weeks'
   },
   {
     'id':'04',
     'post': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolore nemo nam impedit vitae, aliquid sint nihil et neque amet maxime voluptate dicta praesentium id doloremque provident aperiam. Corrupti, sit ullam quaerat dolorem ipsa nesciunt, sapiente pariatur earum cum maxime atque iure architecto cumque minima ipsam nulla reprehenderit magni sequi modi laboriosam natus eveniet non aliquid tempore? Amet beatae natus molestiae? Cupiditate unde, minima esse doloribus itaque ad quo maiores sunt quis veritatis tempora accusantium? Ad expedita et placeat ea minus nemo ipsum perferendis dolor explicabo aliquid, quos a eaque dolorem molestias libero, perspiciatis quisquam ipsa laborum illo nam voluptas adipisci labore sit atque. Porro unde hic veritatis culpa quasi?',
-    'image': 'assets/images/post04.jpg'
+    'image': 'assets/images/post04.jpg',
+    'date':'3 weeks'
   },
   {
     'id':'05',
     'post': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus animi, quod labore quisquam quo enim, adipisci ea laborum harum accusantium blanditiis optio ullam amet ipsum atque odio explicabo corrupti ex commodi! Sequi facilis ea dicta. Numquam nesciunt nobis modi asperiores omnis, natus cupiditate ipsa corporis, itaque adipisci rem aliquid? Possimus aperiam sed, fuga asperiores iusto magni est impedit fugiat voluptate? Alias fuga minima laboriosam quas quos cumque suscipit officia provident iure, amet nulla eaque velit, repudiandae ad eligendi perferendis nemo commodi necessitatibus rerum reiciendis aperiam! Laborum, dicta et voluptatibus doloribus minima nostrum ullam vel fuga, esse error accusamus iusto consectetur?',
-    'image': 'assets/images/post05.jpg'
+    'image': 'assets/images/post05.jpg',
+    'date':'1 month'
   }, 
 ]
--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const classlistEl = document.querySelector('#classlist_el')
+const newsEl = document.querySelector('#news_el')
 
+
+// Classes List
 const getListItem = () => {
     classlistEl.innerHTML = classlist.map(item => 
-    `<hr><h4>${item.classname}</h4><p class='time'>${item.time}</p><p class='description'>${item.description}</p>`
+        `<hr><h4>${item.classname}</h4><p class='time'>${item.time}</p><p class='description'>${item.description}</p>`
     
 ).join("")}
 
 getListItem()
+
+
+
+// News 
+const getNews = () => {
+    newsEl.innerHTML = posts.map(post => 
+        `<div class='card'>
+            <div class='card-header'>
+                <div class='card-logo'>
+                    <img src='assets/logo/icon-sm.png'>
+                </div>
+                <div class='card-text'>
+                    <p class='contributor'>N Yoga</p>
+                    <p><small class='post-data'>${post.date} ago</small></p>
+                </div>
+            </div>
+            <img class='card-image' src='${post.image}'>
+
+        </div>`
+).join("")}
+
+getNews()
